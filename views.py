f97b3ba8e6c35bcd1b71f5040d2d3043ca5776dd
guilhermecarvalhoacc <guilhermeaugustochaves@hotmail.com>
2021-03-11 17:14:28 -0300

    funcionando adicao de cartao

--- views.py
+++ views.py
@@ -1,8 +1,9 @@
 from utils import load_data, load_template,build_response
 import urllib
 import json
-from database import Database 
+from database import Database, Note
 
+database = Database("BANCO_DB")
 
 def index(request):
     # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
@@ -11,30 +12,24 @@ def index(request):
         # Cabeçalho e corpo estão sempre separados por duas quebras de linha
         partes = request.split('\n\n')
         corpo = partes[1]
-        params = {}
         # Preencha o dicionário params com as informações do corpo da requisição
         # O dicionário conterá dois valores, o título e a descrição.
         # Posteriormente pode ser interessante criar uma função que recebe a
         # requisição e devolve os parâmetros para desacoplar esta lógica.
         # Dica: use o método split da string e a função unquote_plus
+        lista_post = []
         for chave_valor in corpo.split('&'):
             lista = urllib.parse.unquote_plus(chave_valor).split("=")
-            params[lista[0]] = lista[1]
+            print(f"ESSA AKI EH A LISTA: {lista}")
             print(f"\n\n {chave_valor}")
+            lista_post.append(lista[1])
             # AQUI É COM VOCÊ
-            
-        database = Database("BANCO_DB")
-        db.add(Note(title='Pão doce', content='Abra o pão e coloque o seu suco em pó favorito.'))
-        db.add(Note(title=None, content='Lembrar de tomar água'))
-
-'''        with open("data/notes.json") as arq_note:
-            data = json.load(arq_note)
-        data.append(params)
-        with open("data/notes.json", "w") as f:
-            json.dump(data,f) #para escrever o novo dicionario no arquivo json'''
-
+        print(f"PARAMS EH: {corpo}")
+        
+        database.add(Note(title= lista_post[0], content= lista_post[1]))
         return build_response(code=303, reason='See Other', headers='Location: /') 
 
+        
     # O RESTO DO CÓDIGO DA FUNÇÃO index CONTINUA DAQUI PARA BAIXO...
     # Cria uma lista de <li>'s para cada anotação
     # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
